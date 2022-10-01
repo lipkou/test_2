c074648ad58f6b6361ded462de701089630f49b8
Lipkou <d.lipkous@gmail.com>
2022-10-01 18:36:48 +0300

    Add all codes

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-print('hi')
-print('hi')
-print('hi')
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from random import randint
+
+app = QApplication([])
+win = QWidget()
+win.setWindowTitle('Тест1')
+win.resize(300, 130)
+win.move(200, 200)
+
+
+lb_text = QLabel('Дізнайтесь переможця:')
+winner = QLabel('???')
+btn = QPushButton('Нажми мене')
+
+
+vline = QVBoxLayout()
+vline.addWidget(lb_text, alignment=Qt.AlignCenter)
+vline.addWidget(winner, alignment=Qt.AlignCenter)
+vline.addWidget(btn, alignment=Qt.AlignCenter)
+
+def rundom_win():
+    lb_text.setText('Переможець:')
+    winner.setText(str(randint(1, 100)))
+
+btn.clicked.connect(rundom_win)
+
+win.setLayout(vline)
+
+
+win.show()
+app.exec_()
